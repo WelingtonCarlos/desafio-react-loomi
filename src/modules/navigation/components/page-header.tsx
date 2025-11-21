df8c6a22d489b884c181b248c29f4de2914f8f31
Welington Filho <wcfilho98@hotmail.com>
2025-11-20 22:50:17 -0300

    feat: create impact by segment chart.

--- src/modules/navigation/components/page-header.tsx
+++ src/modules/navigation/components/page-header.tsx
@@ -8,9 +8,7 @@ interface PageHeaderProps {
 export function PageHeader({ title, subtitle }: PageHeaderProps) {
   return (
     <header className="bg-[#20273E] border-b border-gray-800 py-9 pl-10">
-      <p className="text-sm text-white/50 mb-1">{title}</p>
-      <h1 className="text-xl font-medium text-white">{subtitle ?? title}</h1>
+      <h1 className="text-xl font-medium text-white">{title}</h1>
     </header>
   );
 }
-
